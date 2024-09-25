a0d86b51d2d2f4599126036eebf69dc4c2798ee7
Petr <raknor@seznam.cz>
2024-09-25 14:31:20 +0200

    added 'sun' project for black background

---
+++ sun/for_black_background/index.ts
@@ -0,0 +1,43 @@
+
+const sun=document.getElementById("sun") as HTMLElement;   // eg. the "div" with style "height:400px; width:400px; position:absolute; top:15px; right:15px;"
+
+letThereBeSunOnElement(sun);
+setInitialStyleForElement(sun);
+
+function setInitialStyleForElement (sunElm:HTMLElement) {
+ const lesserViewportSize:number = Math.min(window.innerWidth, window.innerHeight);    // the lesser viewport size in pixels (eg viewport width or viewport height)
+
+ sunElm.style.height= 0.16 * lesserViewportSize + "px";
+ sunElm.style.width= 0.16 * lesserViewportSize + "px";
+ sunElm.style.position= "absolute";
+ sunElm.style.top= 0.008 * lesserViewportSize + "px";
+ sunElm.style.right= 0.008 * lesserViewportSize + "px";
+}
+
+function letThereBeSunOnElement (sunElm:HTMLElement) {
+ const smoothness:number=0.015;                            // time of one animation move, in seconds; for smoother animation lower number, but with bigger computing load
+ const sunriseAndSunsetTime:number=15;                     // how long sun does rise, how long does set, in seconds
+ const yellowDistance:number=25;
+ const blackDistanceAtNight:number=27;                     // yellow is at ${yellowDistance}% (eg. 10%), at night black is at ${blackDistanceAtNight}% (eg. 11%), only ${blackDistanceAtNight - yellowDistance}% (eg. 1%) transition from yellow to black, no much room for sun yellow light
+ const blackDistanceAtNoon:number=100;                     // yellow is at ${yellowDistance}%, at noon black is ${blackDistanceAtNoon}% (eg. 40%), ${blackDistanceAtNight - yellowDistance}% (eg. 30%) transition from yellow to black, giving room for sun yellow light
+ const blackColorStep:number= (blackDistanceAtNoon - blackDistanceAtNight) / (sunriseAndSunsetTime/smoothness);     // counting % of one animation move, the movement of black night
+ let blackColorPosition:number= blackDistanceAtNight;      // animace starts at midnight, with sunRise=true
+ let sunRise:boolean=true;                                 // if sun is about to rise (true), the code adding colorStep, if about to set (false), subtracting
+
+ setInterval(()=>{
+   if(sunRise){
+     blackColorPosition+=blackColorStep;
+     sunElm.style.backgroundImage=`radial-gradient(circle closest-side, red 0% 0%, yellow ${yellowDistance}% ${yellowDistance}%, black ${blackColorPosition}% ${blackDistanceAtNoon}%)`;
+     if (blackColorPosition >= blackDistanceAtNoon) sunRise=false;    // ">=" instead of "===" because declaration of "blackColorStep" involves division, for which rounding may happen, then backward multiplication of blackColorStep will not exactly fit the original number
+   } else {
+     blackColorPosition-=blackColorStep;    
+     sunElm.style.backgroundImage=`radial-gradient(circle closest-side, red 0% 0%, yellow ${yellowDistance}% ${yellowDistance}%, black ${blackColorPosition}% ${blackDistanceAtNoon}%)`;
+     if (blackColorPosition <= blackDistanceAtNight) sunRise=true;
+   }
+ }, smoothness*1000 );
+}
+
+
+
+
+
